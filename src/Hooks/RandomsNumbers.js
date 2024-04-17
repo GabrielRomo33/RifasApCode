89b3a1e2fa6b10eaefa7f6fa770532d61c070d17
GabrielRomo33 <rromo@tiendas-grand.com.mx>
2024-04-16 18:58:02 -0600

    correccion

--- src/Hooks/RandomsNumbers.js
+++ src/Hooks/RandomsNumbers.js
@@ -1,7 +1,7 @@
 
 export const RandomsNumbers = (min, max, cantidad) => {
     var resultados = [];
-    for (var i = 1; i<= cantidad; i++){
+    for (var i = 1; i <= cantidad; i++){
         resultados.push(Math.floor(Math.random() * (max - min + 1) ) );
     }
     return resultados;
